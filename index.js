6d3acc1090d7f881c74ffc0a615036e4ac4a8ac1
2imMinh <nguyentuanminh.cn@gmail.com>
2023-02-15 16:51:13 +0700

    fix bug in index file

--- index.js
+++ index.js
@@ -14,18 +14,19 @@ let b = a.sort((a, b) => {
 console.log(b);
 
 // Bài 2:
-let c = b.map((m) => {
-    return m.name;
-})
+let c = [];
+b.forEach((m) => {
+    let exist = c.find((item) => {
+        return item === m.name;
+    });
+    if (!exist) c.push(m.name);
+});
 
 console.log(c);
 // Bài 3:
-let d = new Array(100).fill(0).map((m, i) => {
-    return m + i, i++;
-});
 let res = 0;
-d.map((m) => {
-    res += (m % 5 == 0) ? m : 0;
+let d = new Array(100).fill(0).map((m, i) => {
+    return res += (m + i) % 5 == 0 ? m + i : 0, m + i, i++;
 });
 console.log(res);
 
@@ -45,7 +46,6 @@ e.forEach((item) => {
     let existingItem = arr.find((outputItem) => {
         return outputItem.name === item.name;
     });
-
     if (existingItem) {
         existingItem.count += item.count;
     } else {
