2b52a3d1617878bbb1e7be19ffcd120ad0e5d490
2imMinh <nguyentuanminh.cn@gmail.com>
2023-02-27 15:53:57 +0700

    fix mysql

--- mydata.sql
+++ mydata.sql
@@ -3,122 +3,130 @@ create database mydata;
 use mydata;
 
 create table users (
-	id bigint auto_increment primary key,
-    username nvarchar(50) not null,
+	id bigint auto_increment,
+    username varchar(16) not null,
     email nvarchar(50) not null,
-    password nvarchar(50) not null,
-    birthday date null,
-    image_url text not null,
-    is_active boolean not null,
+    password nvarchar(10) not null,
+    birthday date,
+    image_url varchar(255),
+    is_active varchar(255) not null,
     created_at timestamp not null,
-    updated_at timestamp not null
+    updated_at timestamp not null,
+    PRIMARY KEY(id),
+    CONSTRAINT users_unique UNIQUE(username, email)
 );
 
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('minh','minh123@gmail.com', '123456', '2001-11-22', 'ascscsca13d',false, '2023-11-23', '2023-11-24');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('hieu','hieu123@gmail.com', '987654', '2001-10-20', 'ascscsca14d',true, '2023-11-25', '2023-11-26');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('dat','dat123@gmail.com', '123456', '2001-09-22', 'ascscsca15d',true, '2023-10-17', '2023-10-20');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('long','long123@gmail.com', '123456', '2001-11-22', 'ascscsca16d',false, '2023-05-12', '2023-05-11');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('nam','nam123@gmail.com', '123456', '2001-11-22', 'ascscsca17d',false, '2023-04-23', '2023-04-24');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('luan','luan123@gmail.com', '123456', '2001-11-22', 'ascscsca17d',false, '2023-04-23', '2023-04-24');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('chung','chung123@gmail.com', '123456', '2001-11-22', 'ascscsca17d',false, '2023-04-23', '2023-04-24');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('loc','loc123@gmail.com', '123456', '2001-11-22', 'ascscsca17d',false, '2023-04-23', '2023-04-24');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('doan','doan123@gmail.com', '123456', '2001-11-22', 'ascscsca17d',false, '2023-04-23', '2023-04-24');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('bach','bach123@gmail.com', '123456', '2001-11-22', 'ascscsca17d',false, '2023-04-23', '2023-04-24');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('phuc','phuc123@gmail.com', '123456', '2001-11-22', 'ascscsca17d',false, '2023-04-23', '2023-04-24');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('manh','manh123@gmail.com', '123456', '2001-11-22', 'ascscsca17d',false, '2023-04-23', '2023-04-24');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('nghia','nghia123@gmail.com', '123456', '2001-11-22', 'ascscsca17d',false, '2023-04-23', '2023-04-24');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('ly','ly123@gmail.com', '123456', '2001-11-22', 'ascscsca17d',false, '2023-04-23', '2023-04-24');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('phuong','phuong123@gmail.com', '123456', '2001-11-22', 'ascscsca17d',false, '2023-04-23', '2023-04-24');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('duyen','duyen123@gmail.com', '123456', '2001-11-22', 'ascscsca17d',false, '2023-04-23', '2023-04-24');
-insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('anh','anh123@gmail.com', '123456', '2001-11-22', 'ascscsca17d',false, '2023-04-23', '2023-04-24');
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('minh','minh123@gmail.com', '123456', '2001-11-22', 'ascscsca13d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('hieu','hieu123@gmail.com', '987654', '2001-10-20', 'ascscsca14d','user', CURRENT_TIMESTAMP() ,CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('dat','dat123@gmail.com', '123456', '2001-09-22', 'ascscsca15d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('long','long123@gmail.com', '123456', '2001-11-22', 'ascscsca16d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('nam','nam123@gmail.com', '123456', '2001-11-22', 'ascscsca17d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('luan','luan123@gmail.com', '123456', '2001-11-22', 'ascscsca17d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('chung','chung123@gmail.com', '123456', '2001-11-22', 'ascscsca17d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('loc','loc123@gmail.com', '123456', '2001-11-22', 'ascscsca17d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('doan','doan123@gmail.com', '123456', '2001-11-22', 'ascscsca17d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('bach','bach123@gmail.com', '123456', '2001-11-22', 'ascscsca17d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('phuc','phuc123@gmail.com', '123456', '2001-11-22', 'ascscsca17d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('manh','manh123@gmail.com', '123456', '2001-11-22', 'ascscsca17d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('nghia','nghia123@gmail.com', '123456', '2001-11-22', 'ascscsca17d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('ly','ly123@gmail.com', '123456', '2001-11-22', 'ascscsca17d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('phuong','phuong123@gmail.com', '123456', '2001-11-22', 'ascscsca17d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('duyen','duyen123@gmail.com', '123456', '2001-11-22', 'ascscsca17d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into users(username, email, password, birthday, image_url, is_active, created_at, updated_at) values ('anh','anh123@gmail.com', '123456', '2001-11-22', 'ascscsca17d','user', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 create table categories (
-	id bigint auto_increment primary key,
-    category_name nvarchar(50) not null,
+	id bigint auto_increment,
+    category_name varchar(50) not null,
     created_at timestamp not null,
-    updated_at timestamp not null
+    updated_at timestamp not null,
+    PRIMARY KEY(id)
 );
 
-insert into categories(category_name, created_at, updated_at) values ('nhom A','2023-11-23', '2023-11-24');
-insert into categories(category_name, created_at, updated_at) values ('nhom B','2023-11-25', '2023-11-26');
-insert into categories(category_name, created_at, updated_at) values ('nhom C','2023-10-17', '2023-10-20');
-insert into categories(category_name, created_at, updated_at) values ('nhom D','2023-05-12', '2023-05-11');
-insert into categories(category_name, created_at, updated_at) values ('nhom E','2023-04-23', '2023-04-24');
+insert into categories(category_name, created_at, updated_at) values ('nhom A',CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into categories(category_name, created_at, updated_at) values ('nhom B',CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into categories(category_name, created_at, updated_at) values ('nhom C',CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into categories(category_name, created_at, updated_at) values ('nhom D',CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into categories(category_name, created_at, updated_at) values ('nhom E',CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 create table companies (
-	id bigint auto_increment primary key,
-    company_name nvarchar(100) not null,
-    company_code nvarchar(50) not null,
+	id bigint auto_increment,
+    company_name nvarchar(50) not null,
+    company_code nvarchar(255) not null,
     created_at timestamp not null,
-    updated_at timestamp not null
+    updated_at timestamp not null,
+    PRIMARY KEY(id),
+    CONSTRAINT companies_unique UNIQUE(company_code)
 );
 
-insert into companies(company_name, company_code, created_at, updated_at) values ('MLVN', '123242', '2022-11-20', '2022-11-26');
-insert into companies(company_name, company_code, created_at, updated_at) values ('ATH', '261532', '2022-05-15', '2022-05-16');
-insert into companies(company_name, company_code, created_at, updated_at) values ('VMO', '215648', '2022-01-05', '2022-01-07');
-insert into companies(company_name, company_code, created_at, updated_at) values ('VTI', '134986', '2022-09-12', '2022-09-13');
-insert into companies(company_name, company_code, created_at, updated_at) values ('Sun*', '132189', '2022-12-25', '2022-12-26');
+insert into companies(company_name, company_code, created_at, updated_at) values ('monstar-lab', '123242', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into companies(company_name, company_code, created_at, updated_at) values ('ATH', '261532', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into companies(company_name, company_code, created_at, updated_at) values ('VMO', '215648', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into companies(company_name, company_code, created_at, updated_at) values ('VTI', '134986', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into companies(company_name, company_code, created_at, updated_at) values ('Sun*', '132189', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 create table projects (
-	id bigint auto_increment primary key,
+	id bigint auto_increment,
     project_name nvarchar(50) not null,
     category_id bigint,
-    projected_spend int not null,
-    projected_variance int not null,
+    projected_spend int,
+    projected_variance int,
     revenue_recognised int,
     created_at timestamp not null,
     updated_at timestamp not null,
     company_id bigint,
-    foreign key (category_id) references categories(id),
-    foreign key (company_id) references companies(id)
+    PRIMARY KEY(id),
+    CONSTRAINT FK_ProjectCategory foreign key (category_id) references categories(id),
+    CONSTRAINT FK_ProjectCompany foreign key (company_id) references companies(id)
 );
 
 insert into projects(project_name, category_id, projected_spend,projected_variance,revenue_recognised,created_at,updated_at,company_id)
-values ('project 1', 1, 120, 1, 150, '2023-08-23', '2023-11-24', 1);
+values ('project 1', 1, 120, 1, 150, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 1);
 insert into projects(project_name, category_id, projected_spend,projected_variance,revenue_recognised,created_at,updated_at,company_id)
-values ('project 2', 2, 200, 1, 250, '2023-06-23', '2023-11-24', 1);
+values ('project 2', 2, 200, 1, 250, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 1);
 insert into projects(project_name, category_id, projected_spend,projected_variance,revenue_recognised,created_at,updated_at,company_id)
-values ('project 3', 3, 150, 1, 190, '2023-01-23', '2023-05-24', 2);
+values ('project 3', 3, 150, 1, 190, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 2);
 insert into projects(project_name, category_id, projected_spend,projected_variance,revenue_recognised,created_at,updated_at,company_id)
-values ('project 4', 4, 250, 1, 280, '2023-02-23', '2023-05-24', 3);
+values ('project 4', 4, 250, 1, 280, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 3);
 insert into projects(project_name, category_id, projected_spend,projected_variance,revenue_recognised,created_at,updated_at,company_id)
-values ('project 5', 5, 100, 1, 150, '2023-07-23', '2023-10-24', 4);
+values ('project 5', 5, 100, 1, 150, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 4);
 
 create table project_users (
-	id bigint auto_increment primary key,
+	id bigint auto_increment,
     project_id bigint,
     user_id bigint, 
     created_at timestamp not null,
     updated_at timestamp not null,
-    foreign key (project_id) references projects(id),
-    foreign key (user_id) references users(id)
+    PRIMARY KEY(id),
+    CONSTRAINT FK_Project foreign key (project_id) references projects(id),
+    CONSTRAINT FK_User foreign key (user_id) references users(id)
 );
 
-insert into project_users(project_id,user_id,created_at,updated_at) values (1, 2, '2023-02-23', '2023-05-24');
-insert into project_users(project_id,user_id,created_at,updated_at) values (1, 1, '2023-02-23', '2023-05-24');
-insert into project_users(project_id,user_id,created_at,updated_at) values (1, 3, '2023-02-23', '2023-05-24');
-insert into project_users(project_id,user_id,created_at,updated_at) values (2, 4, '2023-02-23', '2023-05-24');
-insert into project_users(project_id,user_id,created_at,updated_at) values (2, 5, '2023-02-23', '2023-05-24');
-insert into project_users(project_id,user_id,created_at,updated_at) values (1, 6, '2023-02-23', '2023-05-24');
-insert into project_users(project_id,user_id,created_at,updated_at) values (1, 7, '2023-02-23', '2023-05-24');
-insert into project_users(project_id,user_id,created_at,updated_at) values (1, 8, '2023-02-23', '2023-05-24');
-insert into project_users(project_id,user_id,created_at,updated_at) values (1, 9, '2023-02-23', '2023-05-24');
-insert into project_users(project_id,user_id,created_at,updated_at) values (1, 10, '2023-02-23', '2023-05-24');
-insert into project_users(project_id,user_id,created_at,updated_at) values (1, 11, '2023-02-23', '2023-05-24');
-insert into project_users(project_id,user_id,created_at,updated_at) values (1, 12, '2023-02-23', '2023-05-24');
-insert into project_users(project_id,user_id,created_at,updated_at) values (1, 13, '2023-02-23', '2023-05-24');
-insert into project_users(project_id,user_id,created_at,updated_at) values (1, 14, '2023-02-23', '2023-05-24');
-insert into project_users(project_id,user_id,created_at,updated_at) values (1, 15, '2023-02-23', '2023-05-24');
-insert into project_users(project_id,user_id,created_at,updated_at) values (1, 16, '2023-02-23', '2023-05-24');
+insert into project_users(project_id,user_id,created_at,updated_at) values (1, 2, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into project_users(project_id,user_id,created_at,updated_at) values (1, 1, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into project_users(project_id,user_id,created_at,updated_at) values (1, 3, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into project_users(project_id,user_id,created_at,updated_at) values (2, 4, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into project_users(project_id,user_id,created_at,updated_at) values (2, 5, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into project_users(project_id,user_id,created_at,updated_at) values (1, 6, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into project_users(project_id,user_id,created_at,updated_at) values (1, 7, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into project_users(project_id,user_id,created_at,updated_at) values (1, 8, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into project_users(project_id,user_id,created_at,updated_at) values (1, 9, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into project_users(project_id,user_id,created_at,updated_at) values (1, 10, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into project_users(project_id,user_id,created_at,updated_at) values (1, 11, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into project_users(project_id,user_id,created_at,updated_at) values (1, 12, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into project_users(project_id,user_id,created_at,updated_at) values (1, 13, CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP());
+insert into project_users(project_id,user_id,created_at,updated_at) values (1, 14,CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into project_users(project_id,user_id,created_at,updated_at) values (1, 15, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into project_users(project_id,user_id,created_at,updated_at) values (1, 16, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
 /*3. Viết lệnh sql để có thể lấy thông tin những bản ghi của projects và số lượng user của mỗi projects đó (count user)*/
 select projects.project_name, projects.projected_spend, projects.projected_variance, projects.revenue_recognised, count(project_users.user_id) as 'SL'
 from projects left join project_users on projects.id = project_users.project_id
 group by project_users.project_id 
 
-/*4. Viết lệnh sql để lấy ra danh sách các project của company có company_name = "MLVN"*/
+/*4. Viết lệnh sql để lấy ra danh sách các project của company có company_name = "monstar-lab"*/
 select projects.project_name, projects.projected_spend, projects.projected_variance, projects.revenue_recognised
 from projects join companies on projects.company_id = companies.id
-where companies.company_name = 'MLVN'
+where companies.company_name = 'monstar-lab'
 
 /*5. Viết lệnh sql lấy ra danh sách các công ty có project có project_spend > 100. 
 Viết lệnh sql để lấy ra thông tin của user tham gia vào projects
